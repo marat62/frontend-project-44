43c576d5c66be87e0592c5eec241d39f60f3ce7b
Сафин Марат Салаватович <SafinMS@mog.ent.local>
2024-06-28 15:26:04 +0300

    Stage 25

--- src/games/event.js
+++ src/games/event.js
@@ -1,14 +1,13 @@
-import runGame from '../index.js';
-import { getRandomNumber}  from '../helpers.js'
+import runGame from "../index.js";
+import { getRandomNumber } from "../helpers.js";
 
-
-const description = 'Answer "yes" if the number is even, otherwise answer "no".';
+const description =
+  'Answer "yes" if the number is even, otherwise answer "no".';
 const isEven = (number) => number % 2 === 0;
 
 const generateRound = () => {
- 
   const question = getRandomNumber(1, 100);
-  const correctAnswer = isEven(getRandomNumber) ? 'yes' : 'no';
+  const correctAnswer = isEven(getRandomNumber) ? "yes" : "no";
   return [question, correctAnswer];
 };
 
