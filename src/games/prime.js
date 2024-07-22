8211cf0a08c78d0188f554d5489e1f17eb00e34b
Сафин Марат Салаватович <SafinMS@mog.ent.local>
2024-07-22 10:41:29 +0300

    Stage88

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,6 +1,7 @@
 import runGame from '../index.js';
 
 import { getRandomNumber } from '../helpers.js';
+
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (num) => {
